674b1de164daf60780e14f67368b75efaa9d9f77
Dylan <Dylan@eequ.org>
2021-09-14 15:15:51 +0100

    added navbar on newgame page

--- cypress/integration/user_can_navigate_spec.js
+++ cypress/integration/user_can_navigate_spec.js
@@ -2,6 +2,8 @@ describe('Home page', function() {
   it('can navigate', function() {
     cy.visit('/');
     cy.get('.homepage-nav').find('[href="/newgame"]').click();
-    cy.url().should('include', '/newgame')
+    cy.url().should('include', '/newgame');
+    cy.get('.newgame-nav').find('[href="/"]').click();
+    cy.url().should('eq', 'http://localhost:3030/');
   });
 });
