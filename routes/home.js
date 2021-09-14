351bcefb0e491bf2c3b97c320ec947ceaad6555f
manumoualek <manu@manuels-mbp.home>
2021-09-14 22:34:06 +0100

    amended deletegame route

--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,7 @@ var HomeController = require('../controllers/home');
 router.get('/', HomeController.Index);
 router.get('/newgame', HomeController.New);
 router.post('/newgame', HomeController.Create);
-router.post('deletegame',HomeController.Delete);
+router.post('/deletegame',HomeController.Delete);
 
 
 module.exports = router;
