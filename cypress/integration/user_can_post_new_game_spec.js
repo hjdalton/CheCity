af86d88fb9763fbc98a19f12ab3035b32279689c
ocodner-7 <odaine.codner@gmail.com>
2021-09-14 11:56:48 +0100

    added cypress test for adding a new game

---
+++ cypress/integration/user_can_post_new_game_spec.js
@@ -0,0 +1,21 @@
+var faker = require('faker')
+
+describe('New game', function() {
+  it('can enter details to list a new game', function() {
+    cy.visit('/newgame');
+    var hostname = faker.name.findName();
+    var randomGame = faker.lorem.word();
+    var randomDesc = faker.lorem.words();
+    var randomDate = '2025-05-05'
+    var randomTime = '18:00'
+    var randomAddress = faker.lorem.words();
+
+    cy.get('.new-game').find('[id="hostname"]').type(hostname);
+    cy.get('.new-game').find('[id="gametype"]').type(randomGame);
+    cy.get('.new-game').find('[id="description"]').type(randomDesc);
+    cy.get('.new-game').find('[id="date"]').type(randomDate);
+    cy.get('.new-game').find('[id="time"]').type(randomTime);
+    cy.get('.new-game').find('[id="address"]').type(randomAddress);
+    cy.get('.new-game').submit();
+  })
+})
