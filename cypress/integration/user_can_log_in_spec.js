7c6ef57be2fbc0af10a38e5571aca2dea7f31ce7
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-09-21 10:42:08 +0100

    log in test passing

--- cypress/integration/user_can_log_in_spec.js
+++ cypress/integration/user_can_log_in_spec.js
@@ -1,7 +1,34 @@
+var faker = require('faker');
+
 describe('Log In', function() {
-  it('allows a user to log in with correct credentials',function() {
-    cy.visit('/user/login')
-    cy.get('#login-form').find('[id="username"]').type('test_username')
-    cy.get('#login-form').find('[id="password"]').type('1234')
+  it('does not allow a user to log in with incorrect credentials', function() {
+    cy.visit('/user/login');
+    cy.get('#login-form').find('[id="username"]').type('test_username');
+    cy.get('#login-form').find('[id="password"]').type('1234');
+    cy.get('#login-form').submit();
+
+    cy.url().should('eq', 'http://localhost:3030/user/login');
+
+  })
+
+  it('allows a user to log in with correct credentials', function(){
+    cy.visit('/user/signup');
+    var username = faker.lorem.word();
+    
+    cy.get('#sign-up-form').find('[id="firstname"]').type('cy.test');
+    cy.get('#sign-up-form').find('[id="lastname"]').type('cy.test');
+    cy.get('#sign-up-form').find('[id="username"]').type(username);
+    cy.get('#sign-up-form').find('[id="email"]').type('cy.test@test.com');
+    cy.get('#sign-up-form').find('[id="password"]').type('1234');
+    
+    cy.get('#sign-up-form').submit();
+    //refactor above 
+    cy.visit('/user/login');
+    cy.get('#login-form').find('[id="username"]').type(username);
+    cy.get('#login-form').find('[id="password"]').type('1234');
+    cy.get('#login-form').submit();
+
+    cy.url().should('eq', 'http://localhost:3030/');
+
   })
 })
