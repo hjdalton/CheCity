c83f90090d0a7e9b453b777bd4fa9f076a0714a7
manu <manuelmoualek@gmail.com>
2021-09-23 00:03:27 +0100

    i variable in index.js file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 function disableButton() {
   let buttons = document.getElementsByClassName('bookButton');
   let spaces = document.getElementsByClassName('spaces'); 
-
+  let i;
   
   for (i = 0; i < (spaces.length); i++) {
     if (spaces[i].value === '0') {
