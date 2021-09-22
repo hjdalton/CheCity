874eaf98cb7f33289759f464b9599f3bf8b2743a
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-09-22 14:52:02 +0100

    jest test

--- spec/models/game.spec.js
+++ spec/models/game.spec.js
@@ -7,17 +7,20 @@ describe('Game model', function() {
   
   beforeEach(function(done) {
       mongoose.connection.collections.games.drop(function() {
+
+        
           done();
       });
   });
 
   it('has a game', function() {
-    var game = new Game({ hostname: 'Dylan', gametype: 'some game', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
+    var game = new Game({ hostname: 'Dylan', gametype: 'blitz', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
 
-    expect(game.gametype).toEqual('some game');
+    expect(game.gametype).toEqual('blitz');
   });
 
   it('can list all games', function(done) {
+    
     Game.find(function(err, games) {
       expect(err).toBeNull();
       expect(games).toEqual([]);
@@ -26,7 +29,7 @@ describe('Game model', function() {
   });
 
   it('can save a game', function(done) {
-    var game = new Game({ hostname: 'Dylan', gametype: 'some game', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
+    var game = new Game({ hostname: 'Dylan', gametype: 'blitz', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
 
     game.save(function(err) {
       expect(err).toBeNull();
@@ -42,8 +45,8 @@ describe('Game model', function() {
   });
 
   it('can remove a posted game', function(done) {
-    var game = new Game({ hostname: 'Dylan', gametype: 'some game', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
-    
+    var game = new Game({ hostname: 'Dylan', gametype: 'blitz', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
+
     game.save(function(err) {
       expect(err).toBeNull();
     
@@ -59,23 +62,22 @@ describe('Game model', function() {
     });  
   });
 
-  // it('can delete a post', function(done) {
-  //   var post = new Post({ message: 'check message' });
+  it('can find games by game type', function(done) {
+    var game = new Game({ hostname: 'Dylan', gametype: 'rapid', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
+    var game = new Game({ hostname: 'Player', gametype: 'blitz', description: 'Fast Chess', date: '2018-05-21', time: '18:00', address: '2 Burnt House Farm Cottages' });
     
-  //   post.save(function(err) {
-  //     expect(err).toBeNull();
-    
-  //     Post.findByIdAndRemove({ _id: post._id }, function(err) {
-  //       expect(err).toBeNull();
+    game.save(function(err) {
+      expect(err).toBeNull();
+      
+      console.log(game)
 
-  //       Post.find(function(err, posts) {
-  //         expect(err).toBeNull();
-  //         expect(posts[0]).toBeUndefined();
-  //         done();
-  //       });
-  //     });
-  //   });  
-  // });
-
-
+      Game.find({ gametype: 'blitz' }, function(err, games) {
+        console.log(game)
+        expect(err).toBeNull();
+        expect(games).toEqual(expect.objectContaining({ gametype: "blitz"}));
+        expect(games).not.toContain('rapid')
+        done();
+        })
+      });
+    });
 });
